81529d61a697dcb04e4b2a8a2e5078318346eea5
nish1894 <nishantkumar8466@gmail.com>
2025-02-16 15:08:31 +0530

    Dashboard setup,ORM setup- workbench,Tailwind integration with html

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -1,17 +1,18 @@
 package com.scm.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model; // Import the Model class
 import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RequestMethod;
-// import org.springframework.web.bind.annotation.RequestParam;
-
 
 @Controller
 public class PageController {
 
     @RequestMapping("/home")
-    public String home(){
+    public String home(Model model) { // Add Model as a parameter
         System.out.println("Home page handler");
-        return "home";
+
+        // sending data to view 
+        model.addAttribute("githubRepo", "https://github.com/nish1894/SCM");
+        return "home"; // This should match the Thymeleaf template name (home.html)
     }
 }
