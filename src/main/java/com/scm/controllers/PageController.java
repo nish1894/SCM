6c6a8b5df78b718b468e36c630939cdf59a7677f
nish1894 <nishantkumar8466@gmail.com>
2025-02-16 19:36:21 +0530

    Intregrated thymleaf framents, both static and dynamic

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -3,6 +3,8 @@ package com.scm.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model; // Import the Model class
 import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMethod;
+// import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PageController {
@@ -11,8 +13,22 @@ public class PageController {
     public String home(Model model) { // Add Model as a parameter
         System.out.println("Home page handler");
 
-        // sending data to view 
+        // sending data to view
         model.addAttribute("githubRepo", "https://github.com/nish1894/SCM");
         return "home"; // This should match the Thymeleaf template name (home.html)
     }
+
+    @RequestMapping("/about")
+    public String aboutpage() {
+        System.out.println("About page handler");
+        return "about";
+    }
+
+    @RequestMapping("/services")
+    public String servicespage(Model model) {
+        model.addAttribute("islogin", false);
+        System.out.println("Services page handler");
+        return "services";
+    }
+
 }
