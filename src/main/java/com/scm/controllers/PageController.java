266110c061593cdfbbd0d15d6f5cf9dc40706b7b
nish1894 <nishantkumar8466@gmail.com>
2025-02-17 16:44:10 +0530

    Navigation bar setup- login, signup,logo,navbar

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -1,34 +1,54 @@
 package com.scm.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model; // Import the Model class
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RequestMethod;
-// import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+// Handles page navigation
 public class PageController {
 
     @RequestMapping("/home")
-    public String home(Model model) { // Add Model as a parameter
+    // Home page handler
+    public String home(Model model) {
         System.out.println("Home page handler");
-
-        // sending data to view
         model.addAttribute("githubRepo", "https://github.com/nish1894/SCM");
-        return "home"; // This should match the Thymeleaf template name (home.html)
+        return "home";
     }
 
     @RequestMapping("/about")
+    // About page handler
     public String aboutpage() {
         System.out.println("About page handler");
         return "about";
     }
 
     @RequestMapping("/services")
-    public String servicespage(Model model) {
-        model.addAttribute("islogin", false);
+    // Services page handler
+    public String servicespage() {
         System.out.println("Services page handler");
         return "services";
+    }
+
+    @RequestMapping("/contact")
+    // Contact page handler
+    public String contactpage() {
+        System.out.println("Contact page handler");
+        return "contact";
+    }
+
+    @RequestMapping("/login")
+    // login page handler
+    public String loginpage() {
+        System.out.println("Login page handler");
+        return "login";
+    }
+
+    @RequestMapping("/register")
+    // Register page handler
+    public String registerpage() {
+        System.out.println("Register page handler");
+        return "register";
     }
 
 }
